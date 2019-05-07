e872cccd4a4751a7b972018e11bc6e868dd26101
d471061c <cpastethat@gmail.com>
2019-05-07 10:47:24 +0300

    Add script to package.json

--- backend/index.js
+++ backend/index.js
@@ -8,5 +8,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`)
+    console.log(`Server running at http://localhost:${PORT}`);
 });
