b0bb6ae301d6c78890d928dcee8256744c3ac8a0
stadibo <jesperleonard@gmail.com>
2019-05-22 09:09:15 +0300

    HOTFIX: relative url in frontend for fetching data

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo-hooks'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
+  uri: '/graphql'
 })
 
 ReactDOM.render(
