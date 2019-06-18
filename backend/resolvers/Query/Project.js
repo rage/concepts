d30c67cab3e13a47eb2042cc5592d5e5754af642
d471061c <cpastethat@gmail.com>
2019-06-18 15:30:35 +0300

    add project queries

---
+++ backend/resolvers/Query/Project.js
@@ -0,0 +1,16 @@
+const checkAccess = require('../../accessControl')
+
+const ProjectQueries = {
+  allProjects(root, args, context) {
+    checkAccess(context, { allowStaff: true, allowStudent: true })
+    return context.prisma.projects()
+  },
+  projectById(root, args, context) {
+    checkAccess(context, { allowStaff: true, allowStudent: true })
+    return context.prisma.project({
+      id: args.id
+    })
+  }
+}
+
+module.exports = ProjectQueries
--- backend/resolvers/Query/index.js
+++ backend/resolvers/Query/index.js
@@ -3,11 +3,13 @@ const conceptLink = require('./ConceptLink')
 const course = require('./Course')
 const courseLink = require('./CourseLink')
 const workspace = require('./Workspace')
+const project = require('./Project')
 
 module.exports = {
   ...concept,
   ...conceptLink,
   ...course,
   ...courseLink,
-  ...workspace
+  ...workspace,
+  ...project
 }
