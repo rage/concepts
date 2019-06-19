66258f78ad4e4a7e6035cb9789b9b94034c83f2e
d471061c <cpastethat@gmail.com>
2019-06-19 14:06:04 +0300

    add project and workspace type resolvers

--- backend/index.js
+++ backend/index.js
@@ -20,287 +20,7 @@ const resolvers = {
   },
   Mutation: {
     ...mutations
-
-    // async deleteCourseAsCoursePrerequisite(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   await context.prisma.updateCourse({
-    //     where: { id: args.id },
-    //     data: {
-    //       prerequisiteCourses: {
-    //         disconnect: [{ id: args.prerequisite_id }]
-    //       }
-    //     }
-    //   })
-    //   return context.prisma.course({
-    //     id: args.prerequisite_id
-    //   })
-    // },
-    // async addCourseAsCoursePrerequisite(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   await context.prisma.updateCourse({
-    //     where: { id: args.id },
-    //     data: {
-    //       prerequisiteCourses: {
-    //         connect: [{ id: args.prerequisite_id }]
-    //       }
-    //     }
-    //   })
-    //   return context.prisma.course({
-    //     id: args.prerequisite_id
-    //   })
-    // },
-    // createResourceWithURLs(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   return context.prisma.createResource({
-    //     name: args.name,
-    //     description: args.desc,
-    //     concept: {
-    //       connect: { id: args.concept_id }
-    //     },
-    //     urls: {
-    //       create: args.urls.map(url => {
-    //         return { address: url }
-    //       })
-    //     }
-    //   })
-    // },
-    // createURL(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   return context.prisma.createURL({
-    //     address: args.address,
-    //     resource: { connect: { id: args.resource_id } }
-    //   })
-    // },
-    // createResource(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   return context.prisma.createResource({
-    //     name: args.name,
-    //     description: args.desc,
-    //     concept: { connect: [{ id: args.concept_id }] }
-    //   })
-    // },
-    // createCourse(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   return context.prisma.createCourse({
-    //     name: args.name
-    //   })
-    // },
-    // deleteCourse(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   return context.prisma.deleteCourse({
-    //     id: args.id
-    //   })
-    // },
-    // createConcept(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   const concept = args.desc !== undefined
-    //     ? args.official !== undefined
-    //       ? { name: args.name, description: args.desc, official: args.official }
-    //       : { name: args.name, description: args.desc }
-    //     : args.official !== undefined
-    //       ? { name: args.name, official: args.official }
-    //       : { name: args.name }
-
-    //   return context.prisma.createConcept({
-    //     ...concept,
-    //     courses: { connect: [{ id: args.course_id }] }
-    //   })
-    // },
-    // createConceptAsPrerequisite(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   const concept = args.desc !== undefined
-    //     ? args.official !== undefined
-    //       ? { name: args.name, description: args.desc, official: args.official }
-    //       : { name: args.name, description: args.desc }
-    //     : args.official !== undefined
-    //       ? { name: args.name, official: args.official }
-    //       : { name: args.name }
-
-    //   return context.prisma.createConcept({
-    //     ...concept,
-    //     asPrerequisite: { connect: [{ id: args.course_id }] }
-    //   })
-    // },
-    // createConceptAsLearningObjective(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   const concept = args.desc !== undefined
-    //     ? args.official !== undefined
-    //       ? { name: args.name, description: args.desc, official: args.official }
-    //       : { name: args.name, description: args.desc }
-    //     : args.official !== undefined
-    //       ? { name: args.name, official: args.official }
-    //       : { name: args.name }
-
-    //   return context.prisma.createConcept({
-    //     ...concept,
-    //     asLearningObjective: { connect: [{ id: args.course_id }] }
-    //   })
-    // },
-    // updateConcept(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   let data = {}
-    //   if (args.name !== undefined) {
-    //     data.name = args.name
-    //   }
-    //   if (args.desc !== undefined) {
-    //     data.description = args.desc
-    //   }
-    //   return context.prisma.updateConcept({
-    //     where: { id: args.id },
-    //     data: data
-    //   })
-    // },
-    // updateCourse(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   return context.prisma.updateCourse({
-    //     where: { id: args.id },
-    //     data: { name: args.name }
-    //   })
-    // },
-    // async createConceptAndLinkTo(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   const concept = args.desc !== undefined
-    //     ? args.official !== undefined
-    //       ? { name: args.name, description: args.desc, official: args.official }
-    //       : { name: args.name, description: args.desc }
-    //     : args.official !== undefined
-    //       ? { name: args.name, official: args.official }
-    //       : { name: args.name }
-    //   const createdConcept = await context.prisma.createConcept(concept)
-
-    //   // Link created concept to specified concept
-    //   return args.linkOfficial !== undefined
-    //     ? context.prisma.createLink({
-    //       to: { connect: { id: args.to } },
-    //       from: { connect: { id: createdConcept.id, } },
-    //       official: args.linkOfficial
-    //     })
-    //     : context.prisma.createLink({
-    //       to: { connect: { id: args.to } },
-    //       from: { connect: { id: createdConcept.id } }
-    //     })
-    // },
-    // async deleteConcept(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   await context.prisma.deleteManyLinks({
-
-    //     OR: [
-    //       {
-    //         from: {
-    //           id: args.id
-    //         }
-
-    //       },
-    //       {
-    //         to: {
-    //           id: args.id
-    //         }
-    //       }
-    //     ]
-
-    //   })
-    //   return context.prisma.deleteConcept({
-    //     id: args.id
-    //   })
-    // },
-    // createLink(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   return args.official !== undefined
-    //     ? context.prisma.createLink({
-    //       to: {
-    //         connect: { id: args.to }
-    //       },
-    //       from: {
-    //         connect: { id: args.from, }
-    //       },
-    //       official: args.official
-    //     })
-    //     : context.prisma.createLink({
-    //       to: {
-    //         connect: { id: args.to }
-    //       },
-    //       from: {
-    //         connect: { id: args.from }
-    //       }
-    //     })
-    // },
-    // deleteLink(root, args, context) {
-    //   checkAccess(context, { allowStudent: true })
-    //   return context.prisma.deleteLink({
-    //     id: args.id
-    //   })
-    // }
   },
-  // Concept: {
-  //   linksToConcept(root, args, context) {
-  //     return context.prisma.concept({
-  //       id: root.id
-  //     }).linksToConcept()
-  //   },
-  //   linksFromConcept(root, args, context) {
-  //     return context.prisma.concept({
-  //       id: root.id
-  //     }).linksFromConcept()
-  //   },
-  //   courses(root, args, context) {
-  //     return context.prisma.concept({
-  //       id: root.id
-  //     }).courses()
-  //   },
-  //   asPrerequisite(root, args, context) {
-  //     return context.prisma.concept({
-  //       id: root.id
-  //     }).asPrerequisite()
-  //   },
-  //   asLearningObjective(root, args, context) {
-  //     return context.prisma.concept({
-  //       id: root.id
-  //     }).asLearningObjective()
-  //   },
-  //   resources(root, args, context) {
-  //     return context.prisma.concept({
-  //       id: root.id
-  //     }).resources()
-  //   }
-  // },
-  // Link: {
-  //   to(root, args, context) {
-  //     return context.prisma.link({
-  //       id: root.id
-  //     }).to()
-  //   },
-  //   from(root, args, context) {
-  //     return context.prisma.link({
-  //       id: root.id
-  //     }).from()
-  //   }
-  // },
-  // Course: {
-  //   concepts(root, args, context) {
-  //     return context.prisma.course({
-  //       id: root.id
-  //     }).concepts()
-  //   },
-  //   prerequisiteCourses(root, args, context) {
-  //     return context.prisma.course({
-  //       id: root.id
-  //     }).prerequisiteCourses()
-  //   }
-  // },
-  // Resource: {
-  //   urls(root, args, context) {
-  //     return context.prisma.resource({
-  //       id: root.id
-  //     }).urls()
-  //   }
-  // },
-  // URL: {
-  //   resource(root, args, context) {
-  //     return context.prisma.uRL({
-  //       id: root.id
-  //     }).resource()
-  //   }
-  // }
   Concept: {
     linksToConcept(root, args, context) {
       return context.prisma.concept({
@@ -416,6 +136,65 @@ const resolvers = {
       return context.prisma.uRL({
         id: root.id
       }).resource()
+    }
+  },
+  Workspace: {
+    owner(root, args, context) {
+      return context.prisma.workspace({ 
+        id: root.id
+      }).owner()
+    },
+    defaultCourse(root, args, context) {
+      return context.prisma.workspace({
+        id: root.id
+      }).defaultCourse()
+    },
+    project(root, args, context) {
+      return context.prisma.workspace({
+        id: root.id
+      }).project()
+    },
+    courses(root, args, context) {
+      return context.prisma.workspace({
+        id: root.id
+      }).courses()
+    },
+    conceptLinks(root, args, context) {
+      return context.prisma.workspace({
+        id: root.id
+      }).conceptLinks()
+    },
+    courseLinks(root, args, context) {
+      return context.prisma.workspace({
+        id: root.id
+      }).courseLinks()
+    },
+    concepts(root, args, context) {
+      return context.prisma.workspace({
+        id: root.id
+      }).concepts()
+    }
+  },
+  Project: {
+    owner(root, args, context) {
+      return context.prisma.project({
+        id: root.id
+      }).owner()
+    },
+    participants(root, args, context) {
+      return context.prisma.project({
+        id: root.id
+      }).participants()
+    },
+    workspaces(root, args, context) {
+      return context.prisma.project({
+        id: root.id
+      }).workspaces()
+    },
+    template(root, args, context) {
+      return context.prisma.project({
+        id: root.id
+      }).template()
     }
   }
 }
