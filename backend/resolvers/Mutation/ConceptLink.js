0e5e441dcaac8fe8b9fa3bf6daef2fb8735ff952
d471061c <cpastethat@gmail.com>
2019-06-27 11:55:40 +0300

    add delete and create many concept links to backend

--- backend/resolvers/Mutation/ConceptLink.js
+++ backend/resolvers/Mutation/ConceptLink.js
@@ -21,6 +21,44 @@ const ConceptLink = {
 
     return context.prisma.createConceptLink(data)
   },
+  async createConceptLinks(root, { official, to, from, workspaceId }, context) {
+    checkAccess(context, { allowStudent: true, allowStaff: true })
+    return await from.map(async fromConceptId =>
+      await context.prisma.createConceptLink({
+        to: {
+          connect: { id: to }
+        },
+        from: {
+          connect: { id: fromConceptId }
+        },
+        createdBy: {
+          connect: { id: context.user.id }
+        },
+        workspace: {
+          connect: { id: workspaceId }
+        },
+        official: official !== undefined
+      })
+    )
+  },
+  async deleteConceptLinks(root, args, context) {
+    checkAccess(context, { allowStudent: true, allowStaff: true })
+    const data = await context.prisma.conceptLinks({
+      where: {
+        AND: [
+          { id_in: args.ids },
+          { createdBy: { id: context.user.id } }
+        ]
+      }
+    })
+    await context.prisma.deleteManyConceptLinks({
+      AND: [
+        { id_in: args.ids },
+        { createdBy: { id: context.user.id } }
+      ]
+    })
+    return data
+  },
   async deleteConceptLink(root, args, context) {
     const user = await context.prisma.conceptLink({ id: args.id }).createdBy()
     checkAccess(context, { allowStudent: true, allowStaff: true, verifyUser: true, userId: user.id })
