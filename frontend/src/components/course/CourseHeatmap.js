2af11044aa1c01ddaaacce4eba38f735c2eb4479
d471061c <cpastethat@gmail.com>
2019-06-28 12:12:51 +0300

    center heatmap

--- frontend/src/components/course/CourseHeatmap.js
+++ frontend/src/components/course/CourseHeatmap.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
 import './heatmap.css'
+
 const useStyles = makeStyles(theme => ({
 
 }))
@@ -9,75 +12,79 @@ const CourseHeatmap = (props) => {
   const classes = useStyles()
 
   return (
-    <div className="scrollSyncTable">
-      <table>
-        <thead>
-          <tr>
-            <th></th>
+      <Grid item xs={12}>
+        <Container maxWidth="sm">
+          <div className="scrollSyncTable">
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
 
-            <th><div>HEAD</div></th>
-            <th><div>HEAD</div></th>
-            <th><div>HEAD</div></th>
-            <th><div>HEAD</div></th>
-            <th><div>HEAD</div></th>
+                  <th><div>HEAD</div></th>
+                  <th><div>HEAD</div></th>
+                  <th><div>HEAD</div></th>
+                  <th><div>HEAD</div></th>
+                  <th><div>HEAD</div></th>
 
-            <th><div>HEAD</div></th>
-            <th><div>HEAD</div></th>
-            <th><div>HEAD</div></th>
-            <th><div>HEAD</div></th>
-            <th><div>HEAD</div></th>
-          </tr>
-        </thead>
+                  <th><div>HEAD</div></th>
+                  <th><div>HEAD</div></th>
+                  <th><div>HEAD</div></th>
+                  <th><div>HEAD</div></th>
+                  <th><div>HEAD</div></th>
+                </tr>
+              </thead>
 
-        <tbody>
-          <tr>
-            <th>HEAD</th>
+              <tbody>
+                <tr>
+                  <th>HEAD</th>
 
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
 
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-          </tr>
-          <tr>
-            <th>HEAD</th>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                </tr>
+                <tr>
+                  <th>HEAD</th>
 
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
 
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-          </tr>
-          <tr>
-            <th>HEAD</th>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                </tr>
+                <tr>
+                  <th>HEAD</th>
 
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
 
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-            <td>BODY</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                  <td>BODY</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </Container>
+      </Grid>
   )
 }
 
