46e2aa74523e9d9fad34c724269f1815e8c5793b
d471061c <cpastethat@gmail.com>
2019-07-01 14:32:10 +0300

    add concept poppers to heatmap

--- frontend/src/components/course/CourseHeatmap.js
+++ frontend/src/components/course/CourseHeatmap.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
+
+import Popper from '@material-ui/core/Popper'
+import Fade from '@material-ui/core/Fade'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
 
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
@@ -36,6 +41,9 @@ const useStyles = makeStyles(theme => ({
   },
   paper: {
     maxHeight: '85vh'
+  },
+  popper: {
+    padding: theme.spacing(2)
   }
 }))
 
@@ -74,20 +82,57 @@ const HeaderCell = ({ title }) => {
 
 const TableCell = ({ toCourse, fromCourse, minGradVal, maxGradVal }) => {
   const classes = useStyles()
-  const concepts = toCourse.concepts.map(concept => {
+  const [anchorElement, setAnchorElement] = useState(null)
+  const open = Boolean(anchorElement)
+
+  const conceptsLinked = toCourse.concepts.map(concept => {
     return concept.linksToConcept.filter(conceptLink => {
       return conceptLink.from.courses.find(course => course.id === fromCourse.id)
     }).length
   }).reduce((a, b) => a + b, 0)
+
+  const onlyUnique = (v, i, a) => a.indexOf(v) === i
+  const concepts = toCourse.concepts.map(concept => {
+    return concept.linksToConcept.filter(conceptLink => {
+      return conceptLink.from.courses.find(course => course.id === fromCourse.id)
+    })
+  }).reduce((first, second) => { return first.concat(second) }, [])
+  .map(concept => concept.from.name)
+  .filter(onlyUnique)
+
   const weight = 5;
-  
+
   const mapToGrad = (weight) => {
-    return ((255) / maxGradVal)*(weight)
+    return ((255) / maxGradVal) * (weight)
+  }
+
+  const togglePopper = (event) => {
+    if (concepts.length > 0) {
+      setAnchorElement(anchorElement ? null : event.currentTarget);
+    }
   }
 
   return (
-    <td className={classes.tableCell} style={{ backgroundColor: `RGB(${255 - mapToGrad(concepts)}, ${255 - concepts * weight}, ${255 - mapToGrad(concepts)})` }}>
-    </td>
+    <>
+      <td key={`table-${toCourse.id}-${fromCourse.id}`} className={classes.tableCell} style={{
+        backgroundColor: `RGB(${255 - mapToGrad(conceptsLinked)}, ${255 - conceptsLinked * weight}, ${255 - mapToGrad(conceptsLinked)})`
+      }} onClick={togglePopper}>
+      </td>
+
+      <Popper key={`popper-${toCourse.id}-${fromCourse.id}`} id={toCourse.id} open={open} anchorEl={anchorElement} transition>
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={350}>
+            <Paper key={`paper-${toCourse.id}-${fromCourse.id}`} className={classes.popper}>
+              {
+                concepts.map(concept => (
+                  <Typography key={`typo-${concept}`}> {concept} </Typography>
+                ))
+              }
+            </Paper>
+          </Fade>
+        )}
+      </Popper>
+    </>
   )
 }
 
@@ -98,17 +143,17 @@ const CourseHeatmap = ({ workspaceId }) => {
     variables: { id: workspaceId }
   })
 
-  const maxGradVal = workspaceCourseQuery.data.workspaceById ? 
-  workspaceCourseQuery.data.workspaceById.courses.map(fromCourse => {
-    return workspaceCourseQuery.data.workspaceById.courses.map(toCourse => {
-      return toCourse.concepts.map(concept => {
-        return concept.linksToConcept.filter(conceptLink => {
-          return conceptLink.from.courses.find(course => course.id === fromCourse.id)
-        }).length
-      }).reduce((a, b) => a + b, 0)
+  const maxGradVal = workspaceCourseQuery.data.workspaceById ?
+    workspaceCourseQuery.data.workspaceById.courses.map(fromCourse => {
+      return workspaceCourseQuery.data.workspaceById.courses.map(toCourse => {
+        return toCourse.concepts.map(concept => {
+          return concept.linksToConcept.filter(conceptLink => {
+            return conceptLink.from.courses.find(course => course.id === fromCourse.id)
+          }).length
+        }).reduce((a, b) => a + b, 0)
+      }).reduce((a, b) => a > b ? a : b, 0)
     }).reduce((a, b) => a > b ? a : b, 0)
-  }).reduce((a, b) => a > b ? a : b, 0)
-  : null
+    : null
 
 
   return (
@@ -139,7 +184,7 @@ const CourseHeatmap = ({ workspaceId }) => {
                             <th> {fromCourse.name} </th>
                             {
                               workspaceCourseQuery.data.workspaceById.courses.map(toCourse => (
-                                <TableCell minGradVal={0} maxGradVal={maxGradVal} key={`${fromCourse.id}-${toCourse.id}`} fromCourse={fromCourse} toCourse={toCourse} value={Math.random() * 255}/>
+                                <TableCell minGradVal={0} maxGradVal={maxGradVal} key={`${fromCourse.id}-${toCourse.id}`} fromCourse={fromCourse} toCourse={toCourse} value={Math.random() * 255} />
                               ))
                             }
                           </tr>
