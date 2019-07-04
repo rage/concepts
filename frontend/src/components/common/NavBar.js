0667578be2bf889dbb5545a7bea2d8ea9ae196e8
Tulir Asokan <tulir@maunium.net>
2019-07-04 13:38:38 +0300

    Add eslint import plugin with stock config

--- frontend/src/components/common/NavBar.js
+++ frontend/src/components/common/NavBar.js
@@ -5,12 +5,12 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
-import { Link } from 'react-router-dom'
+import { Link , withRouter } from 'react-router-dom'
 
 // Authentication related imporst
 import Button from '@material-ui/core/Button'
 import { signOut } from '../../lib/authentication'
-import { withRouter } from 'react-router-dom'
+
 
 import { useLoginStateValue } from '../../store'
 
