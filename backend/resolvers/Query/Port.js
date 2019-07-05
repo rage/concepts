aff2a817e2d1e699a8c2226aa0a47c89d165d224
d471061c <cpastethat@gmail.com>
2019-07-05 14:08:33 +0300

    add data export query to backend

--- backend/resolvers/Query/Port.js
+++ backend/resolvers/Query/Port.js
@@ -3,9 +3,40 @@ const { checkAccess } = require('../../accessControl')
 const PortQueries = {
   async exportData(root, args, context) {
     checkAccess(context, { allowStaff: true, allowStudent: true })
-    const workspace = await context.prisma.workspace({
+
+    const query = `
+    query($id : ID!) {
+      workspace(where: {
+        id: $id
+      }) {
+        id
+        name
+        courses {
+          name
+          concepts {
+            name
+            description
+            prerequisites: linksFromConcept {
+              to {
+                name
+                official
+              }
+            }
+          }
+          prerequisites: linksFromCourse {
+            to {
+              name
+            }
+          }
+        }
+      }
+    }
+    `
+    const result = await context.prisma.$graphql(query, {
       id: args.workspaceId
     })
+    
+    const workspace = result['workspace']
 
     if (!workspace) {
       throw new Error("Workspace not found")
@@ -14,10 +45,40 @@ const PortQueries = {
     // Create json from workspace
     const jsonData = {
       'workspaceId': workspace.id,
-      'workspace': workspace.name
+      'workspace': workspace.name,
+      'courses': []
+    }
+    
+    for (const course of workspace['courses']) {
+      const courseData = {
+        'name': course['name'],
+        'concepts': [],
+        'prerequisites': []
+      }
+      for (const prerequisiteCourse of course['prerequisites']) {
+        courseData['prerequisites'].push(prerequisiteCourse['to']['name'])
+      }
+      for (const concept of course['concepts']) {
+        const conceptData = {
+          'name': concept['name'],
+          'description': concept['description'],
+          'prerequisites': []
+        }
+        for (const prerequisiteConcept of concept['prerequisites'] ) {
+          // Add concept prerequisite to concept
+          conceptData['prerequisites'].push({
+            'name': prerequisiteConcept['to']['name'],
+            'official': prerequisiteConcept['to']['official']
+          })
+        }
+        // Add concept to course
+        courseData['prerequisites'].push(conceptData)
+      }
+
+      // Add course prerequisites
+      jsonData['courses'].push(courseData)
     }
 
-    console.log(workspace.courses)
 
     return JSON.stringify(jsonData)
   }
