dd9a7673c3e85b3c3204011a88b12d3c916d1492
Tulir Asokan <tulir@maunium.net>
2019-07-10 11:06:00 +0300

    Add comments to masonry hacks

--- frontend/src/components/course/Masonry.js
+++ frontend/src/components/course/Masonry.js
@@ -9,9 +9,17 @@ const styles = theme => ({
     boxSizing: 'border-box'
   },
   columnWrapper: {
-    overflow: 'hidden',
+    // These hacks could be replaced with display: inline-block, but then Chrome doesn't wrap the
+    // items into many columns nicely. The fix to that is to just use the default display: block,
+    // but that causes all browsers to chop our fancy boxes in the middle.
+
+    // This is the standard way to prevent chopping up boxes, but browsers don't support it.
     breakInside: 'avoid-column',
+    // This does the same thing, but is chrome-specific.
     columnBreakInside: 'avoid',
+    // This magically fixes it for Firefox.
+    overflow: 'hidden',
+    // Since we don't use inline-block, text-align won't work, so we use flex to center-align.
     display: 'flex',
     justifyContent: 'center'
   }
