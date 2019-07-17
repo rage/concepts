931ae83dd1e5c7f40ea0c8daf3b356945e1c3780
Tulir Asokan <tulir@maunium.net>
2019-07-17 15:35:17 +0300

    Update graph view parameters

--- frontend/src/components/graph/GraphView.js
+++ frontend/src/components/graph/GraphView.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import Container from '@material-ui/core/Container'
 import { withStyles } from '@material-ui/core'
 import {
   WORKSPACE_COURSES_AND_CONCEPTS
@@ -8,10 +7,10 @@ import client from '../../apollo/apolloClient'
 
 import vis from 'vis'
 
-const styles = theme => ({
+const styles = () => ({
   graph: {
-    width: '100%',
-    height: '100%'
+    gridArea: 'content',
+    overflow: 'hidden'
   }
 })
 
@@ -24,6 +23,10 @@ const GraphView = ({ classes, workspaceId }) => {
     // create a network
     var container = document.getElementById('graph')
     var options = {
+      layout: {
+        randomSeed: 1,
+        improvedLayout: true
+      },
       nodes: {
         shape: 'box',
         shadow: true
@@ -31,6 +34,16 @@ const GraphView = ({ classes, workspaceId }) => {
       edges: {
         width: 2,
         shadow: true
+      },
+      physics: {
+        barnesHut: {
+          centralGravity: 0.2,
+          springLength: 200
+        },
+        forceAtlas2Based: {
+          avoidOverlap: 1,
+          springLength: 200
+        }
       }
     }
     new vis.Network(container, {
@@ -51,38 +64,48 @@ const GraphView = ({ classes, workspaceId }) => {
       const courses = response.data.workspaceById.courses
 
       // Initialize data for states
-      const conceptNodeData = []
-      const conceptLinkData = []
+      const nodes = []
+      const links = []
 
       for (const course of courses) {
         for (const concept of course.concepts) {
-          conceptNodeData.push({
+          nodes.push({
             id: concept.id,
             label: concept.name,
             group: course.id
           })
 
           for (const conceptLink of concept.linksToConcept) {
-            conceptLinkData.push({
+            links.push({
               from: conceptLink.from.id,
               to: concept.id,
-              arrows: 'to'
+              arrows: 'to',
+              physics: conceptLink.from.courses[0].id === course.id
             })
           }
+          links.push({
+            from: course.id,
+            to: concept.id,
+            dashes: true,
+            length: 50
+          })
         }
+        nodes.push({
+          id: course.id,
+          label: course.name,
+          group: course.id,
+          shape: 'ellipse',
+          mass: 1,
+          hidden: true
+        })
       }
-      init(conceptNodeData, conceptLinkData)
+      init(nodes, links)
     }
     // Fill data and initialize the graph
     fillData()
   }, [])
 
-  return (
-    <Container>
-      <div className={classes.graph} id='graph'>
-      </div>
-    </Container>
-  )
+  return <div className={classes.graph} id='graph' />
 }
 
 
