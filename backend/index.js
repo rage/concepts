9702ef67c9547d7e66af88c66e2e9f4a1e28326e
Tulir Asokan <tulir@maunium.net>
2019-07-22 09:36:21 +0300

    Revert switch to nexus-prisma
    This reverts commits 0f1dc1888dd8655214031e9c0123c027ce827a9d and 14696005b3fae3905faa4c94b55049a58445decd.

--- backend/index.js
+++ backend/index.js
@@ -8,38 +8,12 @@ const { prisma } = require('./generated/prisma-client')
 const { GraphQLServer } = require('graphql-yoga')
 const express = require('express')
 
-const datamodelInfo = require("./generated/nexus-prisma")
-const { makePrismaSchema, prismaObjectType } = require('nexus-prisma')
 const { authenticate } = require('./middleware/authentication')
 const { logError } = require('./errorLogger')
 
 const queries = require('./resolvers/Query')
 const mutations = require('./resolvers/Mutation')
 const types = require('./resolvers/Type')
-
-const { rule, shield, and, or, not } = require('graphql-shield')
-
-const Query = prismaObjectType({
-  name: "Query",
-  definition: (type) => type.prismaFields(['*'])
-})
-
-const Mutation = prismaObjectType({
-  name: "Mutation",
-  definition: (type) => type.prismaFields(['*'])
-})
-
-const schema = makePrismaSchema({
-  types: [Query, Mutation],
-  prisma: {
-    datamodelInfo,
-    client: prisma
-  },
-  outputs: {
-    schema: path.join(__dirname, "./generated/schema.graphql"),
-    typegen: path.join(__dirname, "./generated/nexus.d.ts"),
-  }
-})
 
 const resolvers = {
   Query: {
@@ -58,50 +32,14 @@ const options = {
   formatError: logError
 }
 
-const Role = {
-  VISITOR: 'VISITOR',
-  GUEST: 'GUEST',
-  STUDENT: 'STUDENT',
-  STAFF: 'STAFF',
-  ADMIN: 'ADMIN'
-}
-
-const isAdmin = rule()(async (parent, args, ctx, info) => {
-  return ctx.role === Role.ADMIN
-})
-
-const isGuest = rule()(async (parent, args, ctx, info) => {
-  return ctx.role === Role.GUEST
-})
-
-const isStudent = rule()(async (parent, args, ctx, info) => {
-  return ctx.role === Role.STUDENT
-})
-
-const isStaff = rule()(async (parent, args, ctx, info) => {
-  return ctx.role === Role.STAFF
-})
-
-const isVisitor = rule()(async (parent, args, ctx, info) => {
-  return ctx.role === Role.VISITOR
-})
-
-const permissions = shield({
-  Query: {
-  },
-  Mutation: {
-  },
-  User: isAdmin,
-})
-
 const server = new GraphQLServer({
-  typeDefs: './generated/schema.graphql',
-  schema,
+  typeDefs: './schema.graphql',
+  resolvers,
   context: req => ({
     prisma,
     ...req
   }),
-  middlewares: [authenticate, permissions]
+  middlewares: [authenticate]
 })
 
 if (process.env.ENVIRONMENT === 'production') {
