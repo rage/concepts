ae3c609ce2bca5ecdb6a218cbf7e6925a00b4d91
Tulir Asokan <tulir@maunium.net>
2019-08-02 12:19:22 +0300

    Move WorkspaceList to user

--- frontend/src/views/user/UserViewContent.js
+++ frontend/src/views/user/UserViewContent.js
@@ -13,7 +13,7 @@ import {
   UPDATE_PROJECT
 } from '../../graphql/Mutation'
 import { WORKSPACES_FOR_USER, PROJECTS_FOR_USER } from '../../graphql/Query'
-import WorkspaceList from '../workspace/WorkspaceList'
+import WorkspaceList from './WorkspaceList'
 import ProjectList from '../project/ProjectList'
 
 const useStyles = makeStyles(() => ({
