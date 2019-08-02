0e2d2cfc22315ba9aa1b2c500756bc80cb5926f9
d471061c <cpastethat@gmail.com>
2019-08-02 11:46:29 +0300

    Add imports to indeces

---
+++ frontend/src/dialogs/concept/index.js
@@ -0,0 +1,4 @@
+export { default as ConceptAdditionDialog } from './ConceptAdditionDialog'
+export { default as ConceptEditingDialog } from './ConceptEditingDialog'
+export { default as useCreateConceptDialog } from './useCreateConceptDialog'
+export { default as useEditConceptDialog } from './useEditConceptDialog'
---
+++ frontend/src/dialogs/project/index.js
@@ -0,0 +1,7 @@
+export { default as ProjectCreationDialog } from './ProjectCreationDialog'
+export { default as ProjectEditingDialog } from './ProjectEditingDialog'
+export { default as ProjectSharingDialog } from './ProjectSharingDialog'
+
+export { default as TemplateCreationDialog } from './TemplateCreationDialog'
+export { default as useCreateTemplateDialog } from './useCreateTemplateDialog'
+export { default as useEditTemplateDialog } from './useEditTemplateDialog'
