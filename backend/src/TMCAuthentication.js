bce872246b5195f6cb97fa82e91931354b74002d
Tulir Asokan <tulir@maunium.net>
2019-08-02 09:32:28 +0300

    Add comment

--- backend/src/TMCAuthentication.js
+++ backend/src/TMCAuthentication.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 
+// TODO PR this into tmc-client-js
 async function userDetails(accessToken) {
   const res = await axios.get(
     'https://tmc.mooc.fi/api/v8/users/current?show_user_fields=true',
