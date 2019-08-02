b1fd935cf8cdf9bf8f57caf49078c5d1cc3de85b
Tulir Asokan <tulir@maunium.net>
2019-08-02 12:47:01 +0300

    Revert "Rename workspacelist to userworkspacelist"
    This reverts commit 483388f60a094fd3358da9dab6ef7e2aaa4175a3.

--- frontend/src/views/user/UserViewContent.js
+++ frontend/src/views/user/UserViewContent.js
@@ -13,7 +13,7 @@ import {
   UPDATE_PROJECT
 } from '../../graphql/Mutation'
 import { WORKSPACES_FOR_USER, PROJECTS_FOR_USER } from '../../graphql/Query'
-import WorkspaceList from './UserWorkspaceList'
+import WorkspaceList from './WorkspaceList'
 import ProjectList from '../project/ProjectList'
 
 const useStyles = makeStyles(() => ({
