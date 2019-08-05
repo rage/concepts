b3e903e383d5dd08afbe176ef81be620673da94d
stadibo <jesperleonard@gmail.com>
2019-08-05 13:51:23 +0300

    Remove unused dialog components

--- frontend/src/dialogs/course/index.js
+++ frontend/src/dialogs/course/index.js
@@ -1,4 +1,2 @@
 export { default as useCreateCourseDialog } from './useCreateCourseDialog'
 export { default as useEditCourseDialog } from './useEditCourseDialog'
-export { default as CourseCreationDialog } from './CourseCreationDialog'
-export { default as CourseEditingDialog } from './CourseEditingDialog'
