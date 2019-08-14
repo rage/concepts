b035354e56833c74650aafba8a742dfc3dfb9867
d471061c <cpastethat@gmail.com>
2019-08-14 10:49:09 +0300

    add id generation

--- backend/src/resolvers/Mutation/Merge.js
+++ backend/src/resolvers/Mutation/Merge.js
@@ -1,5 +1,5 @@
-const { checkAccess, Role, Privilege } = require('../../accessControl')
-
+const crypto = require('crypto')
+ 
 const clonedWorkspacesQuery = `
 query($id : ID!) {
   project(where: {
@@ -39,16 +39,18 @@ const secretCharset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234
 const makeSecret = length => Array.from({ length },
   () => secretCharset[Math.floor(Math.random() * secretCharset.length)]).join('')
 
-const getCourseId = (courseName) => {
-  return ''
-}
-
-const getConceptId = (conceptName) => {
-  return ''
-}
-
 const MergeMutations = {
   async mergeWorkspaces(root, { projectId }, context) {
+    const time = Date.now().toString()
+    
+    const getCourseId = (courseName) => {
+      return crypto.createHash('sha1').update(time).update(courseName).digest('base64').substr(0, 25)
+    }
+    
+    const getConceptId = (courseName, conceptName) => {
+      return crypto.createHash('sha1').update(time).update(courseName).update(conceptName).digest('base64').substr(0, 25)
+    }
+    
     // Check if project has an active template
     const activeTemplate = context.prisma.project({
       id: projectId
@@ -94,7 +96,7 @@ const MergeMutations = {
         // Add concepts
         for (const concept of course.concepts) {
           if (!(concept.name in mergedWorkspaceData.concepts)) {
-            mergedWorkspaceData.concepts[concept.name] = true
+            mergedWorkspaceData.concepts[concept.name] = course.name
             mergedWorkspaceData.courses[course.name].push({
               name: concept.name,
               description: concept.description
@@ -102,7 +104,7 @@ const MergeMutations = {
           }
 
           mergedWorkspaceData.conceptLinks[concept.name] = {}
-          concept.linkFromConcept.forEach(conceptLink => {
+          concept.linksFromConcept.forEach(conceptLink => {
             if (!mergedWorkspaceData.conceptLinks[concept.name][conceptLink.to.name]) {
               mergedWorkspaceData.conceptLinks[concept.name][conceptLink.to.name] = 1
             } else {
@@ -140,7 +142,7 @@ const MergeMutations = {
           createdBy: { connect: { id: context.user.id }},
           concepts: {
             create: mergedWorkspaceData.courses[courseName].map(concept => ({
-              id: getConceptId(concept.name),
+              id: getConceptId(courseName, concept.name),
               name: concept.name,
               description: concept.description,
               createdBy: { connect: { id: context.user.id } },
@@ -163,8 +165,8 @@ const MergeMutations = {
         create: Object.keys(mergedWorkspaceData.conceptLinks).map(fromConcept => {
           return Object.keys(mergedWorkspaceData.conceptLinks[fromConcept]).map(toConcept => ({
             createdBy: { id: context.user.id },
-            from: getConceptId(fromConcept),
-            to: getConceptId(toConcept)
+            from: getConceptId( mergedWorkspaceData.concepts[fromConcept], fromConcept),
+            to: getConceptId(mergedWorkspaceData.concepts[toConcept], toConcept)
             // weight: mergedWorkspaceData.conceptLinks[fromConcept][toConcept]
           }))
         }).reduce((a, b) => a.concat(b), [])
