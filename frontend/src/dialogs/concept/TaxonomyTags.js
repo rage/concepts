6bdc8729542396a34eef1a5cc44a48cdeffa0a81
Tulir Asokan <tulir@maunium.net>
2019-08-27 09:50:45 +0300

    Improve bloom's colors

--- frontend/src/dialogs/concept/TaxonomyTags.js
+++ frontend/src/dialogs/concept/TaxonomyTags.js
@@ -1,8 +1,8 @@
 export default {
   'REMEMBER': { value: 'REMEMBER', label: 'Remember', color: 'purple', type: 'bloom' },
   'UNDERSTAND': { value: 'UNDERSTAND', label: 'Understand', color: 'blue', type: 'bloom' },
-  'APPLY': { value: 'APPLY', label: 'Apply', color: 'cyan', type: 'bloom' },
+  'APPLY': { value: 'APPLY', label: 'Apply', color: 'darkturquoise', type: 'bloom' },
   'ANALYZE': { value: 'ANALYZE', label: 'Analyze', color: 'green', type: 'bloom' },
-  'EVALUATE': { value: 'EVALUATE', label: 'Evaluate', color: 'yellow', type: 'bloom' },
-  'CREATE': { value: 'CREATE', label: 'Create', color: 'orange', type: 'bloom' }
+  'EVALUATE': { value: 'EVALUATE', label: 'Evaluate', color: 'orange', type: 'bloom' },
+  'CREATE': { value: 'CREATE', label: 'Create', color: 'maroon', type: 'bloom' }
 }
