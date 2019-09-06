2ba13ccc6a5c7f414b6bb577916a245a28ea880d
stadibo <jesperleonard@gmail.com>
2019-09-06 14:47:35 +0300

    Separate pointsAPI into own module

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const { logError } = require('./errorLogger')
 const queries = require('./resolvers/Query')
 const mutations = require('./resolvers/Mutation')
 const types = require('./resolvers/Type')
-const { userDetails } = require('./TMCAuthentication')
+const pointsAPI = require('./pointsAPI')
 
 const resolvers = {
   Query: {
@@ -42,78 +42,8 @@ const server = new GraphQLServer({
   middlewares: [authenticate]
 })
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
-server.express.get('/projects/:pid/courses/:cid/progress', async (req, res) => {
-  const { pid, cid } = req.params
-  const token = getTokenFrom(req)
-  if (!token) return res.status(401).send('Token missing')
-  if (!pid || !cid) return res.status(401).send('Project or course id missing')
-  let userData
-  try {
-    userData = await userDetails(token)
-  } catch (e) {
-    return res.status(401).send('Token invalid')
-  }
-  const tmcId = userData.id
-  const data = await prisma.$graphql(`
-    query($projectId: ID!,$courseId: ID!) {
-      project(where: { id: $projectId }) {
-        id
-        activeTemplate {
-          id
-          pointGroups(where: {course: {id: $courseId} }) {
-            id name
-            maxPoints pointsPerConcept
-            course { id }
-          }
-        }
-      }
-    }
-  `,
-  {
-    projectId: pid,
-    courseId: cid
-  })
-  const project = data.project
-  const userProgress = []
-  const pointGroups = (project && project.activeTemplate) && project.activeTemplate.pointGroups
-  if (project && pointGroups) {
-    for (const pointGroup of pointGroups) {
-      console.log(pointGroup)
-      const existingCompletions = await prisma.pointGroup({ id: pointGroup.id }).completions()
-        .$fragment(`
-      fragment CompletionWithUser on Completion {
-        id
-        conceptAmount
-        user { tmcId }
-      }
-      `)
-      const completionForUser = existingCompletions.find(completion =>
-        completion.user.tmcId === tmcId)
-      const userPoints = completionForUser.conceptAmount * pointGroup.pointsPerConcept
-      const progress = pointGroup.maxPoints < userPoints ? 1.00 : pointGroup.maxPoints / userPoints
-      const nPoints = pointGroup.maxPoints < userPoints ? pointGroup.maxPoints : userPoints
-
-      /* eslint-disable camelcase */
-      userProgress.push({
-        group: pointGroup.name,
-        progress,
-        n_points: nPoints,
-        max_points: pointGroup.maxPoints
-      })
-      /* eslint-enable camelcase */
-    }
-  }
-
-  return res.json(userProgress)
-})
+// Points for completions
+server.express.get('/projects/:pid/courses/:cid/progress', pointsAPI)
 
 if (process.env.ENVIRONMENT === 'production') {
   const BUILD_PATH = path.join(__dirname, '../../frontend/build')
