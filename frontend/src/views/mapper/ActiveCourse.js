029eda7a7411f8dd3299ec0485ebccbe4e8cc132
Tulir Asokan <tulir@maunium.net>
2019-09-18 14:28:15 +0300

    Fix style details

--- frontend/src/views/mapper/ActiveCourse.js
+++ frontend/src/views/mapper/ActiveCourse.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef } from 'react'
 import { withRouter } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import {
--- frontend/src/views/mapper/Course.js
+++ frontend/src/views/mapper/Course.js
@@ -2,11 +2,7 @@ import React, { useEffect } from 'react'
 import { withRouter } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import { Button, Card, CardHeader, CardContent, List, IconButton } from '@material-ui/core'
-import {
-  Edit as EditIcon,
-  Lock as LockedIcon,
-  LockOpen as LockOpenIcon
-} from '@material-ui/icons'
+import { Edit as EditIcon, Lock as LockedIcon, LockOpen as LockOpenIcon } from '@material-ui/icons'
 import { useMutation } from 'react-apollo-hooks'
 
 import { Concept } from './concept'
@@ -93,7 +89,7 @@ const Course = ({
 
   useEffect(() => {
     infoBox.redrawIfOpen('mapper', 'CREATE_CONCEPT_PREREQ')
-  }, [course.concepts])
+  }, [infoBox, course.concepts])
 
   return (
     <Card elevation={0} className={classes.root}>
