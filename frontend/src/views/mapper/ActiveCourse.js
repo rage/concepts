95c145e4ef3ede16cb7ac0723ff09b16c993bf33
Tulir Asokan <tulir@maunium.net>
2019-09-20 12:11:54 +0300

    Remove unused import

--- frontend/src/views/mapper/ActiveCourse.js
+++ frontend/src/views/mapper/ActiveCourse.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import {
   Button, Paper, Select, MenuItem, InputBase, List, IconButton
