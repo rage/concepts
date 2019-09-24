ac63152f6464fb5bc6fdddc7b70f77f3b4e18252
Tulir Asokan <tulir@maunium.net>
2019-09-24 14:26:07 +0300

    Add explicit useBabelPlugin in frontend config-overrides

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,9 +1,11 @@
 /* eslint-env node */
 const path = require('path')
 
-const { useBabelRc, babelInclude, override } = require('customize-cra')
+const { addBabelPlugin, useBabelRc, babelInclude, override } = require('customize-cra')
 
 module.exports = override(
   useBabelRc(),
   babelInclude([path.resolve('src'), path.resolve(__dirname, '../backend/src')]),
+  // useBabelRc doesn't seem to work for the include
+  addBabelPlugin('@babel/plugin-proposal-optional-chaining'),
 )
