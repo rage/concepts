5a74e73061d114dbd68efc011809952f48e595c2
d471061c <cpastethat@gmail.com>
2019-10-22 14:01:15 +0300

    migrate to apollo

--- frontend/src/apollo/apolloClient.js
+++ frontend/src/apollo/apolloClient.js
@@ -1,11 +1,44 @@
 import { createRef } from 'react'
-import ApolloClient, { InMemoryCache } from 'apollo-boost'
-import { split } from 'apollo-link'
+import ApolloClient from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { split, ApolloLink, Observable } from 'apollo-link'
 import { createHttpLink } from 'apollo-link-http'
 import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
 
 let requestsInFlight = 0
+
+const authenticationLink = new ApolloLink((operation, forward) => {
+  const isMutation = Boolean(operation.query.definitions
+    .find(def => def.operation === 'mutation'))
+
+  operation.setContext({
+    headers: {
+      'X-Concepts-IsMutation': isMutation.toString()
+    }
+  })
+  const rawData = window.localStorage.currentUser
+  if (rawData) {
+    let data
+    try {
+      data = JSON.parse(rawData)
+    } catch (e) {
+      window.localStorage.clear()
+      console.error(e)
+    }
+    if (data.token) {
+      operation.setContext({
+        headers: {
+          authorization: `Bearer ${data.token}`,
+          'X-Concepts-IsMutation': isMutation.toString()
+        }
+      })
+    }
+  }
+
+  return forward(operation)
+})
+
 
 const wsLink = new WebSocketLink({
   uri: 'ws://localhost:8080/graphql',
@@ -13,10 +46,29 @@ const wsLink = new WebSocketLink({
 })
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:8080/graphql'
+  uri: 'http://localhost:8080/graphql',
+  credentials: 'include',
+  fetch: async (uri, options) => {
+    const isMutation = options.headers['X-Concepts-IsMutation'] === 'true'
+    if (isMutation) {
+      if (requestsInFlight === 0 && savingIndicator.current) {
+        savingIndicator.current.innerText = 'Saving...'
+      }
+      requestsInFlight++
+      const result = await fetch(uri, options)
+      requestsInFlight--
+      if (requestsInFlight === 0 && savingIndicator.current) {
+        // eslint-disable-next-line require-atomic-updates
+        savingIndicator.current.innerText = 'All changes saved.'
+      }
+      return result
+    } else {
+      return fetch(uri, options)
+    }
+  }
 })
 
-const link = split(
+const linkSplit = split(
   ({ query }) => {
     const { kind, operation } = getMainDefinition(query)
     return kind === 'OperationDefinition' && operation === 'subscription'
@@ -26,57 +78,13 @@ const link = split(
 )
 export const savingIndicator = createRef()
 
+const link = authenticationLink.concat(linkSplit)
+
 const client = new ApolloClient({
   link,
   cache: new InMemoryCache({
     dataIdFromObject: o => o.id
-  }),
-  request: (operation) => {
-    const isMutation = Boolean(operation.query.definitions
-      .find(def => def.operation === 'mutation'))
-
-    operation.setContext({
-      headers: {
-        'X-Concepts-IsMutation': isMutation.toString()
-      }
-    })
-    const rawData = window.localStorage.currentUser
-    if (rawData) {
-      let data
-      try {
-        data = JSON.parse(rawData)
-      } catch (e) {
-        window.localStorage.clear()
-        console.error(e)
-      }
-      if (data.token) {
-        operation.setContext({
-          headers: {
-            authorization: `Bearer ${data.token}`,
-            'X-Concepts-IsMutation': isMutation.toString()
-          }
-        })
-      }
-    }
-  },
-  fetch: async (resource, init) => {
-    const isMutation = init.headers['X-Concepts-IsMutation'] === 'true'
-    if (isMutation) {
-      if (requestsInFlight === 0 && savingIndicator.current) {
-        savingIndicator.current.innerText = 'Saving...'
-      }
-      requestsInFlight++
-      const result = await fetch(resource, init)
-      requestsInFlight--
-      if (requestsInFlight === 0 && savingIndicator.current) {
-        // eslint-disable-next-line require-atomic-updates
-        savingIndicator.current.innerText = 'All changes saved.'
-      }
-      return result
-    } else {
-      return fetch(resource, init)
-    }
-  }
+  })
 })
 
 export default client
