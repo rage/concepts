8cb5b2d9a43af844dd92f9ee6afb12e9a090d116
Tulir Asokan <tulir@maunium.net>
2019-10-25 14:42:49 +0300

    Add sentry error monitoring

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/browser'
 
 import ConceptsWrapper from './Wrapper'
 
+Sentry.init({ dsn: 'https://7f1caeb36aff4af79d556609a311b69a@sentry.io/1796166' })
 ReactDOM.render(<ConceptsWrapper />, document.getElementById('root'))
