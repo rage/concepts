a9d64edb03f9ef0185428546d2273350c4395f47
Tulir Asokan <tulir@maunium.net>
2019-10-30 10:04:56 +0200

    Disable Sentry in dev builds

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,5 +4,10 @@ import * as Sentry from '@sentry/browser'
 
 import ConceptsWrapper from './Wrapper'
 
-Sentry.init({ dsn: 'https://7f1caeb36aff4af79d556609a311b69a@sentry.io/1796166' })
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV !== 'development') {
+  Sentry.init({ dsn: 'https://7f1caeb36aff4af79d556609a311b69a@sentry.io/1796166' })
+} else {
+  console.log('Dev build; not enabling Sentry')
+}
 ReactDOM.render(<ConceptsWrapper />, document.getElementById('root'))
