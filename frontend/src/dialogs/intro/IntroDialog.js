9b20552ac3d60f370d49ee29e1e92175c1c63d2c
stadibo <jesperleonard@gmail.com>
2019-11-07 13:54:27 +0200

    Make introDialog per view (not stepped)

--- frontend/src/dialogs/intro/IntroDialog.js
+++ frontend/src/dialogs/intro/IntroDialog.js
@@ -1,103 +1,64 @@
-import React, { useState, useRef } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import React, { useState } from 'react'
 import {
   Dialog as MuiDialog, DialogActions, DialogContent, DialogContentText,
-  DialogTitle, FormControlLabel, Button, Checkbox, IconButton, FormControl, Typography
+  DialogTitle, FormControlLabel, Button, Checkbox, FormControl
 } from '@material-ui/core'
-import {
-  NavigateNext as NextIcon, NavigateBefore as PrevIcon
-} from '@material-ui/icons'
 
 import introContent from '../../static/introContent'
 
-const useStyles = makeStyles(() => ({
-  root: {
+const mapObject = (obj, func) => Object.fromEntries(Object.entries(obj)
+  .map(([k, v], ...args) => [k, func(v, ...args)]))
 
-  },
-  button: {
-    color: 'black',
-    borderColor: 'black'
-  },
-  invisible: {
-    visibility: 'hidden'
-  }
-}))
+introContent.originalViews = introContent.views
+introContent.viewMaps = mapObject(introContent.views,
+  view => Object.fromEntries(view.map(item => [item.id, item])))
 
 const blankState = () => ({
   open: false,
-  type: 'workspace',
-  step: 0
+  currentView: null,
+  currentGuide: null
 })
 
-const IntroDialogContent = ({ currentCardContent }) => (
-  <DialogContent>
+const IntroDialogContent = ({ currentCardContent }) => (<>
     <DialogTitle>{currentCardContent.title}</DialogTitle>
-    <DialogContentText>{currentCardContent.description}</DialogContentText>
-    {currentCardContent.video && <video controls autoPlay muted width='100%' height='100%'>
-      <source src={currentCardContent.video} type='video/webm' />
-    </video>}
-  </DialogContent>
-)
+    <DialogContent>
+      <DialogContentText>{currentCardContent.description}</DialogContentText>
+      {currentCardContent.video &&
+      <video key={currentCardContent.id} controls autoPlay muted width='100%' height='100%'>
+        <source src={currentCardContent.video} type='video/webm' />
+      </video>}
+    </DialogContent>
+  </>)
 
 const IntroDialog = ({ contextRef }) => {
   const [state, setState] = useState(blankState())
   const [inputState, setInputState] = useState({
     noShowAgain: false
   })
-  const classes = useStyles()
 
-  const currentIntroContent = state.type ? introContent.types[state.type] : null
-
-  const local = {
-    get hasNext() {
-      return currentIntroContent?.length > state.step + 1
-    },
-    get hasPrev() {
-      return state.step > 0
-    }
-  }
+  const currentCardContent = state.currentView && state.currentGuide ?
+    introContent.viewMaps[state.currentView][state.currentGuide] : null
 
   const closeDialog = () => {
     // Other cleanup
-    setState({ ...state, open: false })
+    setState({ open: false })
+    setInputState({ noShowAgain: false })
   }
 
   contextRef.current.closeDialog = closeDialog
 
-  contextRef.current.openDialog = (type) => {
+  contextRef.current.openDialog = (currentView, currentGuide) => {
     // Other state changes
-    setState({ ...state, open: true, type })
+    setState({ ...state, open: true, currentView, currentGuide })
   }
-
-  const showNext = () => {
-    setState((prevState) => ({ ...prevState, step: prevState.step + 1 }))
-  }
-  const showPrev = () => {
-    setState((prevState) => ({ ...prevState, step: prevState.step - 1 }))
-  }
-
-  console.log(currentIntroContent)
 
   return (
     <MuiDialog maxWidth='xl' open={state.open} onClose={closeDialog}>
       {
-        currentIntroContent && state.type &&
-        <IntroDialogContent currentCardContent={currentIntroContent[state.step]} />
+        currentCardContent && <IntroDialogContent currentCardContent={currentCardContent} />
       }
       <DialogActions>
         <div>
-          <IconButton
-            className={`${classes.button} ${!local.hasPrev ? classes.invisible : ''}`}
-            onClick={showPrev} disabled={!local.hasPrev}
-          >
-            <PrevIcon />
-          </IconButton>
-          <IconButton
-            className={`${classes.button} ${!local.hasNext ? classes.invisible : ''}`}
-            onClick={showNext} disabled={!local.hasNext}
-          >
-            <NextIcon />
-          </IconButton>
           <FormControl
             style={{ verticalAlign: 'middle', marginRight: '12px' }}
             key='Do not show again'
@@ -106,7 +67,10 @@ const IntroDialog = ({ contextRef }) => {
               control={
                 <Checkbox
                   checked={inputState.noShowAgain}
-                  onChange={evt => setInputState({ ...inputState, noShowAgain: evt.target.checked })}
+                  onChange={evt => setInputState({
+                    ...inputState,
+                    noShowAgain: evt.target.checked
+                  })}
                   value='Do not show again'
                   color='primary'
                 />
