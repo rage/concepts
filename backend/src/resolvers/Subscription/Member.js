6390d8df7b5df875dcdbd233f47d36489d8b60f9
d471061c <cpastethat@gmail.com>
2019-11-13 12:03:13 +0200

    add subscriptions to member update

--- backend/src/resolvers/Subscription/Member.js
+++ backend/src/resolvers/Subscription/Member.js
@@ -5,7 +5,9 @@ import {
   MEMBER_JOINED_PROJECT, 
   MEMBER_LEFT_PROJECT,
   MEMBER_JOINED_WORKSPACE,
-  MEMBER_LEFT_WORKSPACE
+  MEMBER_LEFT_WORKSPACE,
+  MEMBER_UPDATED_PROJECT,
+  MEMBER_UPDATED_WORKSPACE
 } from './config/channels'
 
 export const memberJoinedWorkspace = {
@@ -28,3 +30,14 @@ export const memberLeftProject = {
     (payload, variables) => payload.participant.projectId === variables.projectId)
 }
 
+// Member updated in project
+export const memberUpdatedProject = {
+  subscribe: withFilter(() => pubsub.asyncIterator(MEMBER_UPDATED_PROJECT),
+    (payload, variables) => payload.participant.projectId === variables.projectsId)
+}
+
+// Member updated in workspace
+export const memberUpdatedWorkspace = {
+  subscribe: withFilter(() => pubsub.asyncIterator(MEMBER_UPDATED_WORKSPACE),
+    (payload, variables) => payload.participant.workspaceId === variables.workspaceId)
+}
