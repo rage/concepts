1529fbc3cd1313f30eeb7fa37fdb0a8d18d798c5
Tulir Asokan <tulir@maunium.net>
2019-11-28 15:00:56 +0200

    Make subscription resolvers generic

--- backend/src/resolvers/Subscription/Member.js
+++ backend/src/resolvers/Subscription/Member.js
@@ -1,48 +1,13 @@
-import { withFilter } from 'graphql-subscriptions'
+import { canViewProject } from '../../util/accessControl'
+import { makeSubscriptionResolvers } from './subscriptionUtil'
 
-import { canViewWorkspace, canViewProject } from '../../util/accessControl'
-import { pubsub } from './config'
-import {
-  PROJECT_MEMBER_CREATED,
-  PROJECT_MEMBER_DELETED,
-  WORKSPACE_MEMBER_CREATED,
-  WORKSPACE_MEMBER_DELETED,
-  PROJECT_MEMBER_UPDATED,
-  WORKSPACE_MEMBER_UPDATED
-} from './config/channels'
+export const {
+  workspaceMemberCreated, workspaceMemberUpdated, workspaceMemberDeleted
+} = makeSubscriptionResolvers(
+  'workspace member', ['create', 'update', 'delete'])
 
-export const workspaceMemberCreated = {
-  subscribe: withFilter(() => pubsub.asyncIterator(WORKSPACE_MEMBER_CREATED),
-    async (payload, variables, ctx) => await canViewWorkspace(ctx, variables.workspaceId)
-      && payload.workspaceMemberCreated.workspaceId === variables.workspaceId)
-}
-
-export const workspaceMemberDeleted = {
-  subscribe: withFilter(() => pubsub.asyncIterator(WORKSPACE_MEMBER_DELETED),
-    async (payload, variables, ctx) => await canViewWorkspace(ctx, variables.workspaceId)
-      && payload.workspaceMemberDeleted.workspaceId === variables.workspaceId)
-}
-
-export const workspaceMemberUpdated = {
-  subscribe: withFilter(() => pubsub.asyncIterator(WORKSPACE_MEMBER_UPDATED),
-    async (payload, variables, ctx) => await canViewWorkspace(ctx, variables.workspaceId)
-      && payload.workspaceMemberUpdated.workspaceId === variables.workspaceId)
-}
-
-export const projectMemberCreated = {
-  subscribe: withFilter(() => pubsub.asyncIterator(PROJECT_MEMBER_CREATED),
-    async (payload, variables, ctx) => await canViewProject(ctx, variables.projectId)
-      && payload.projectMemberCreated.projectId === variables.projectId)
-}
-
-export const projectMemberDeleted = {
-  subscribe: withFilter(() => pubsub.asyncIterator(PROJECT_MEMBER_DELETED),
-    async (payload, variables, ctx) => await canViewProject(ctx, variables.projectId)
-      && payload.projectMemberDeleted.projectId === variables.projectId)
-}
-
-export const projectMemberUpdated = {
-  subscribe: withFilter(() => pubsub.asyncIterator(PROJECT_MEMBER_UPDATED),
-    async (payload, variables, ctx) => await canViewProject(ctx, variables.projectId)
-      && payload.projectMemberUpdated.projectId === variables.projectsId)
-}
+export const {
+  projectMemberCreated, projectMemberUpdated, projectMemberDeleted
+} = makeSubscriptionResolvers(
+  'project member', ['create', 'update', 'delete'],
+  'projectId', 'projectId', canViewProject)
--- backend/src/resolvers/Subscription/Workspace.js
+++ backend/src/resolvers/Subscription/Workspace.js
@@ -1,17 +1,4 @@
-import { withFilter } from 'graphql-subscriptions'
+import { makeSubscriptionResolvers } from './subscriptionUtil'
 
-import { canViewWorkspace } from '../../util/accessControl'
-import { pubsub } from './config'
-import { WORKSPACE_UPDATED, WORKSPACE_DELETED } from './config/channels'
-
-export const workspaceUpdated = {
-  subscribe: withFilter(() => pubsub.asyncIterator(WORKSPACE_UPDATED),
-    async (payload, variables, ctx) => await canViewWorkspace(ctx, variables.workspaceId)
-      && payload.workspaceUpdated.workspaceId === variables.workspaceId)
-}
-
-export const workspaceDeleted = {
-  subscribe: withFilter(() => pubsub.asyncIterator(WORKSPACE_DELETED),
-    async (payload, variables, ctx) => await canViewWorkspace(ctx, variables.workspaceId)
-      && payload.workspaceDeleted.workspaceId === variables.workspaceId)
-}
+export const { workspaceUpdated, workspaceDeleted } = makeSubscriptionResolvers(
+  'workspace', ['update', 'delete'])
