f7322b9784a8d525ceb8ac8fecc91487c751f92b
Tulir Asokan <tulir@maunium.net>
2020-01-14 14:48:27 +0200

    Switch NODE_ENV check as it's no longer defined in dev

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import './lib/titleCase'
 import ConceptsWrapper from './Wrapper'
 
 // eslint-disable-next-line no-undef
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV === 'production') {
   Sentry.init({ dsn: 'https://7f1caeb36aff4af79d556609a311b69a@sentry.io/1796166' })
 } else {
   console.log('Dev build; not enabling Sentry')
