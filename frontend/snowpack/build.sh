d21471559209cd97595a0d1e72790d15a8b84f55
Tulir Asokan <tulir@maunium.net>
2020-01-14 14:15:38 +0200

    Improve scripts

--- frontend/snowpack/build.sh
+++ frontend/snowpack/build.sh
@@ -4,7 +4,7 @@ babel -d dist/ ../src/ \
   --presets @babel/preset-react \
   --plugins @babel/plugin-proposal-optional-chaining \
   --plugins @babel/plugin-proposal-class-properties \
-  --plugins $(npm -g root)/snowpack/assets/babel-plugin.js \
+  --plugins ./node_modules/snowpack/assets/babel-plugin.js \
   --plugins ./babel-plugin.js
 cat ../public/index.html \
   | sed 's/%PUBLIC_URL%//g' \
--- frontend/snowpack/watch.sh
+++ frontend/snowpack/watch.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 babel --verbose --watch -d dist/ ../src/ \
   --presets @babel/preset-react \
   --plugins @babel/plugin-proposal-optional-chaining \
   --plugins @babel/plugin-proposal-class-properties \
-  --plugins $(npm -g root)/snowpack/assets/babel-plugin.js \
+  --plugins ./node_modules/snowpack/assets/babel-plugin.js \
   --plugins ./babel-plugin.js
