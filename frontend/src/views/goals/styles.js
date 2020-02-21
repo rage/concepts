4153b6d6692aaf7f11cd0e117b169b77ced0fbf6
Tulir Asokan <tulir@maunium.net>
2020-02-21 14:09:18 +0200

    Add some space between goal view halfs

--- frontend/src/views/goals/styles.js
+++ frontend/src/views/goals/styles.js
@@ -3,14 +3,14 @@ import { makeStyles } from '@material-ui/core/styles'
 export const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
-    maxWidth: '1280px',
+    maxWidth: '1408px',
     margin: '0 auto',
     display: 'grid',
     overflow: 'hidden',
     gridGap: '16px',
-    gridTemplate: `"header  header"   56px
-                   "courses goals" 1fr
-                  / 1fr     1fr`,
+    gridTemplate: `"header  header header" 56px
+                   "courses space  goals"  1fr
+                  / 1fr     128px  1fr`,
     '@media screen and (max-width: 1312px)': {
       width: 'calc(100% - 32px)'
     }
@@ -24,6 +24,12 @@ export const useStyles = makeStyles(theme => ({
     display: 'flex',
     flexDirection: 'column',
     overflow: 'hidden'
+  },
+  courses: {
+    gridArea: 'courses'
+  },
+  goals: {
+    gridArea: 'goals'
   },
   list: {
     overflow: 'auto'
