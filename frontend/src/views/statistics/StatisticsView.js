bc5d37cc474da1d5b60a2fc601675f150ce7daf7
d471061c <cpastethat@gmail.com>
2020-03-02 13:38:49 +0200

    add statistics card draft

--- frontend/src/views/statistics/StatisticsView.js
+++ frontend/src/views/statistics/StatisticsView.js
@@ -1,10 +1,117 @@
 import React from 'react'
+import { Grid, Typography, Card, CardContent, CardHeader } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { 
+  People as PeopleIcon, 
+  Note as NoteIcon,
+  Share as ShareIcon
+ } from '@material-ui/icons'
 
-const StatisticsView = ({ projectId }) => {
+const useStyles = makeStyles(theme => ({
+  cardStyle: {
+    width: '100%',
+    maxWidth: '1280px',
+    margin: '0 auto'
+  },
+  root: {
+    width: '90%',
+    display: 'grid',
+    margin: '0 auto',
+    gridTemplate: `"statistics" 60px
+                   "graph" 1fr`
+  },
+  statistics: {
+    display: 'grid',
+    gridGap: '12px',
+    gridTemplate: `"participants  concepts links" 1fr
+                   / 72px 72px 72px`
+  },
+  participants: {
+    gridArea: 'participants'
+  },
+  concepts: {
+    gridArea: 'concepts'
+  },
+  links: {
+    gridArea: 'links'
+  },
+  graph: {
+    gridArea: 'graph'
+  }
+}))
+
+const Participants = ({ amount }) => {
+  const classes = useStyles()
 
   return (
-    <div>
-    </div>
+    <Grid container direction="row" spacing={1}
+      alignItems="center" className={classes.participants}>
+      <Grid item>
+        <PeopleIcon/>
+      </Grid>
+      <Grid item>
+        <Typography variant='h6'>
+          { amount }
+        </Typography>
+      </Grid>
+    </Grid>
+  )
+}
+
+const Concepts = ({ amount }) => {
+  const classes = useStyles()
+
+
+  return (
+    <Grid container direction="row" spacing={1}
+      alignItems="center" className={classes.concepts}>
+      <Grid item>
+        <NoteIcon/>
+      </Grid>
+      <Grid item>
+        <Typography variant='h6'>
+          { amount }
+        </Typography>
+      </Grid>
+    </Grid>
+  )
+}
+
+const Links = ({ amount }) => {
+  const classes = useStyles()
+
+  return (
+    <Grid container direction="row" spacing={1}
+      alignItems="center" className={classes.links}>
+      <Grid item>
+        <ShareIcon/>
+      </Grid>
+      <Grid item>
+        <Typography variant='h6'>
+          { amount }
+        </Typography>
+      </Grid>
+    </Grid>
+  )
+}
+
+const StatisticsView = ({ projectId }) => {
+  const classes = useStyles()
+
+  return (
+    <Card elevation={0} className={classes.cardStyle}>
+      <CardHeader title="Statistics"/>
+      <CardContent className={classes.root}>
+        <div className={classes.statistics}>
+          <Participants amount={220}/>
+          <Concepts amount={12}/> 
+          <Links amount={358}/>
+        </div>
+        <div className={classes.graph}>
+          
+        </div>
+      </CardContent>
+    </Card>
   )
 }
 
