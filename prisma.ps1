75689d4cbb5a0bd27e0d8916ce23216da739c942
d471061c <cpastethat@gmail.com>
2020-03-16 11:36:57 +0200

    Update prisma.ps1
    Co-Authored-By: Tulir Asokan <tulir@maunium.net>

--- prisma.ps1
+++ prisma.ps1
@@ -2,7 +2,7 @@ $mode, $commands = $args
 if ($mode -eq "prod") {
     docker exec concepts_concepts_1 prisma $commands
 } elseif ($mode -eq "dev") {
-    ddocker exec concepts_backend_1 prisma $commands
+    docker exec concepts_backend_1 prisma $commands
 } else {
     docker exec concepts_backend_1 prisma $args
 }
