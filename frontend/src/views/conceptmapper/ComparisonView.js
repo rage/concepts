428e42dfc3edc271893389d81162497f5420093c
d471061c <cpastethat@gmail.com>
2020-05-15 12:57:34 +0300

    add graphs to graph containers

--- frontend/src/views/conceptmapper/ComparisonView.js
+++ frontend/src/views/conceptmapper/ComparisonView.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core'
 import { useQuery } from '@apollo/react-hooks'
 import { WORKSPACE_COMPARISON } from '../../graphql/Query'
 
 import LoadingBar from '../../components/LoadingBar'
 import { compareConcepts } from './conceptMapComparison'
+
+import cytoscape from 'cytoscape'
+import klay from 'cytoscape-klay'
+
+cytoscape.use(klay)
 
 const useStyles = makeStyles(theme => ({
     wrapper: {
@@ -29,13 +34,75 @@ const useStyles = makeStyles(theme => ({
 }))
 
 
-const GraphContainer = ({ workspace }) => {
+const GraphContainer = ({ workspace, courseId }) => {
     const { graphWrapper } = useStyles()
+    const graphRef = useRef(null)
+
+    const state = useRef({
+        network: null,
+        nodes: null,
+        edges: null
+    })
+
+    const initGraph = () => {
+        const cur = state.current
+        const course = workspace.courses.find(course => course.id == courseId)
+        if (course == null) return (<div> Course not found </div>)
+
+        cur.nodes = []
+        cur.edges = []
+
+        for (const concept of course.concepts) {
+            cur.nodes.push({
+                group: 'nodes',
+                data: {
+                    id: concept.id,
+                    shape: 'rectangle',
+                    label: concept.name,
+                    display: 'element',
+                    type:'concept'
+                }
+            })
+            for (const conceptLink of concept.linksToConcept) {
+                cur.edges.push({
+                    group: 'edges',
+                    data: {
+                        id: conceptLink.from.id + concept.id,
+                        source: conceptLink.from.id,
+                        type: 'edge',
+                        display: 'element',
+                        target: concept.id
+                    }
+                })
+            }
+        }
+
+        cur.network = cytoscape({
+            container: graphRef.current,
+            elements: [].concat(cur.nodes, cur.edges),
+            style: [
+                {
+                    selector: 'node[type = "concept"]',
+                    style: {
+                        label: 'data(label)',
+                        shape: 'round-rectangle',
+                        width: 'label',
+                        height: 'label',
+                        'text-wrap': 'wrap',
+                        'text-max-width': '200px',
+                        'text-valign': 'center',
+                        padding: '10px',
+                        display: 'data(display)'
+                    }
+              }
+            ]
+        })
+    }
+
+    useEffect(() => initGraph(), [])
 
     return (
-        <div className={graphWrapper} >
-            <p> TBA </p> 
-        </div>
+        <div className={graphWrapper} ref={graphRef}/>
     )
 }
 
@@ -58,16 +125,18 @@ const ComparisonView = ({ urlPrefix, workspaceId, compWorkspaceId }) => {
     const compareCourses = (course, compCourse) => {
         return compareConcepts(course.concepts, compCourse.concepts) 
     }
+
     const distance  = compareCourses(workspace.courses[0], compWorkspace.courses[0])
+
     return (
         <div className={wrapper}>
             <div className={compPercentageWrapper}>
                 <h1> Total distance: { distance.toFixed(2) } </h1>  
             </div>
             <div className={containerWrapper}>
-                <GraphContainer workspace={workspace}/>
+                <GraphContainer workspace={workspace} courseId={workspace.courses[0].id}/>
                 <GraphSplitter/>
-                <GraphContainer workspace={compWorkspace}/>
+                <GraphContainer workspace={compWorkspace} courseId={compWorkspace.courses[0].id}/>
             </div>
         </div>
     )
