ef8c9df2b52db5170d5c7363f1f3bab89941d729
d471061c <cpastethat@gmail.com>
2020-07-16 10:06:03 +0300

    add edit workspace name test

--- frontend/cypress/integration/guest.spec.js
+++ frontend/cypress/integration/guest.spec.js
@@ -23,4 +23,12 @@ describe('As a guest I can', () => {
         cy.workspaceListContains("guestWorkspace", false)
     })
 
+    it("can rename workspace", () => {
+        // Rename mock template
+        cy.get(':nth-child(1) > .MuiListItemSecondaryAction-root > .MuiButtonBase-root').click()
+        cy.get('[aria-label="Edit"]').click()
+        cy.get('.MuiInputBase-input').type("2")
+        cy.get('[type="submit"] > .MuiButton-label').click()
+        cy.workspaceListContains("Mock template2", true)
+    })
 })
